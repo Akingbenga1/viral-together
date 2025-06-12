a544e8c4e75a488b7fb67f6b726953741df1ea93
Akinbami Gbenga <akinbami.gbenga@gmail.com>
2025-06-12 11:59:32 +0100

    chore: add cors for middlesware

--- app/main.py
+++ app/main.py
@@ -12,6 +12,14 @@ app = FastAPI(swagger_ui_parameters={
     "deepLinking": False
 })
 
+  app.add_middleware(
+       CORSMiddleware,
+       allow_origins=["*"],  # Adjust this to your frontend's origin
+       allow_credentials=True,
+       allow_methods=["*"],
+       allow_headers=["*"],
+   )
+
 
 # Configure logging
 logging.basicConfig(
